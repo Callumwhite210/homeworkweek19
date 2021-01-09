34f80a598252492ed7f5d4020faba95c90494a78
Callum White <callumwhite210@gmail.com>
2021-01-09 12:58:10 +1030

    Updates

---
+++ precache-manifest.4ed7fa6a01ad93fede46adcf44c32300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45351052464a03333f0d8a6c673d0e0f",
+    "url": "/homeworkweek19/index.html"
+  },
+  {
+    "revision": "d4b6cb86d65cac1c3140",
+    "url": "/homeworkweek19/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "187b78dacbbea1c8fbf7",
+    "url": "/homeworkweek19/static/css/main.8c316816.chunk.css"
+  },
+  {
+    "revision": "d4b6cb86d65cac1c3140",
+    "url": "/homeworkweek19/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "187b78dacbbea1c8fbf7",
+    "url": "/homeworkweek19/static/js/main.4808fd9b.chunk.js"
+  },
+  {
+    "revision": "81a7025d41c5455571a7",
+    "url": "/homeworkweek19/static/js/runtime~main.22aac15d.js"
+  }
+]);
